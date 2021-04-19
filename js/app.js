3c79ea704dc41c5891d67ad45c86691aa1145486
Federico Tanco <fedetanco22@gmail.com>
2021-04-19 00:23:02 -0300

    Animation cards

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-const spanHero = document.querySelector(".Hero__Span");
-
 document.addEventListener("DOMContentLoaded", function () {
 	const titulosHero = document.querySelectorAll(".features");
 	titulosHero.forEach((item) => item.classList.add("features--active"));
+
+	/* const titulo = document.querySelector(".features-titulo");
+	titulo.classList.add("features--active"); */
+});
+
+document.addEventListener("scroll", function () {
+	const features = document.querySelectorAll(".cards");
+	const section = document.querySelector(".features-icons");
+	if (window.scrollY > section.offsetTop - window.innerHeight / 1) {
+		features.forEach((item) => item.classList.add("features--active"));
+	}
 });
 
 document.addEventListener("scroll", function () {
